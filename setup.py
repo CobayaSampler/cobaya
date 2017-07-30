6a0975385b2ebb22e695347e8c90253aa1b01b45
JesusTorrado <JesusTorrado@users.noreply.github.com>
2017-07-30 23:34:37 +0200

    general: setup.py

--- setup.py
+++ setup.py
@@ -13,27 +13,23 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='cobaya',
     version='0.1',
-    description='A Monte Carlo sampler ready for Cosmology',
+    description='Bayesian Analysis in Cosmology',
     long_description=long_description,
     url='https://github.com/JesusTorrado/cobaya',
     author='Jesus Torrado and Antony Lewis',
-    license='LGPL [PROVISIONAL!]',
+    license='LGPL (code, except where stated otherwise) and GFDL (docs)',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Environment :: Console',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Astronomy',
-        'License :: LGPL [PROVISIONAL!]',
-        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Programming Language :: Python :: 2.7',
-#        'Programming Language :: Python :: 3',
     ],
-    keywords='cosmology montecarlo sampling',
+    keywords='montecarlo sampling cosmology',
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=['numpy', 'scipy >= 0.18', 'matplotlib', 'pandas', 'pyyaml',
-                      #'getdist' # not for now: using modified version
-                      #mpi4py
-                     ],
+    install_requires=['numpy', 'scipy >= 0.18', 'matplotlib', 'pandas', 'pyyaml'],
     entry_points={
         'console_scripts': [
             'cobaya-run=cobaya.run:run_script',
