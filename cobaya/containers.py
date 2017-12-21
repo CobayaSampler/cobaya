91dc6764742ec04beed653f6edfa23f46a7a2cc7
Antony Lewis <antony@cosmologist.info>
2017-12-21 10:46:12 +0000

    py3 compat

--- cobaya/containers.py
+++ cobaya/containers.py
@@ -35,7 +35,7 @@ log = logging.getLogger(__name__)
 requirements_file_path = os.path.join(_modules_path, _requirements_file)
 help_file_path = os.path.join(_modules_path, _help_file)
 
-base_recipe = ur"""
+base_recipe = r"""
 # OS -------------------------------------------------------------------------
 FROM ubuntu:xenial
 # POST -----------------------------------------------------------------------
@@ -195,7 +195,7 @@ def create_docker_image(filenames, MPI_version=None):
     echos_help = "RUN "+" && \\ \n    ".join(
         [r'echo "%s" >> %s'%(line, help_file_path)
          for line in image_help("docker").split("\n")])
-    recipe = ur"""
+    recipe = r"""
     FROM cobaya/base_mpich_%s:latest
     %s
     RUN cobaya-install %s --path %s --just-code --force --no-progress-bars
