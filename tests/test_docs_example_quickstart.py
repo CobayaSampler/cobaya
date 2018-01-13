9f759d0a662a1878d893def2633d8a1085730bde
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-01-13 19:13:27 +0000

    fixed last commit for py3

--- tests/test_docs_example_quickstart.py
+++ tests/test_docs_example_quickstart.py
@@ -16,13 +16,13 @@ docs_img_folder = os.path.join(docs_folder, "img")
 def test_consistency():
     info_yaml = yaml_load_file(os.path.join(docs_src_folder, "gaussian.yaml"))
     global_vars = {}  # globar vars used by the "interactive example"
-    execfile(os.path.join(docs_src_folder, "create_info.py"), global_vars)
+    exec(open(os.path.join(docs_src_folder, "create_info.py")).read(), global_vars)
     assert is_equal_info(info_yaml, global_vars["info"]), (
         "Inconsistent info between yaml and insteractive.")
     # temporarily change workign directory to load the example yaml file
     cwd = os.getcwd()
     os.chdir(docs_src_folder)
-    execfile(os.path.join(docs_src_folder, "load_info.py"), global_vars)
+    exec(open(os.path.join(docs_src_folder, "load_info.py")).read(), global_vars)
     os.chdir(cwd)
     assert is_equal_info(info_yaml, global_vars["info_from_yaml"]), (
         "Inconsistent info between interactive and *loaded* yaml.")
