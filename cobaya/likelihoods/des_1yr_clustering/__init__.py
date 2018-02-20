a2b95763e478c0654a6efa8b3f88a514830ce575
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-02-20 21:27:43 +0000

    DES likelihood

---
+++ cobaya/likelihoods/des_1yr_clustering/__init__.py
@@ -0,0 +1,4 @@
+from cobaya.likelihoods._des_prototype import _des_prototype
+
+class des_1yr_clustering(_des_prototype):
+    pass
---
+++ tests/test_cosmo_des_1yr.py
@@ -0,0 +1,105 @@
+from __future__ import print_function
+import numpy as np
+
+from cobaya.conventions import _path_install, _theory, _sampler, _likelihood, _params
+from cobaya.run import run
+from cobaya.yaml import yaml_load
+from cosmo_common import baseline_cosmology_classy_extra
+
+
+test_params_shear = """
+params:
+  # wl_photoz_errors
+  DES_DzS1:  0.002
+  DES_DzS2: -0.015
+  DES_DzS3:  0.007
+  DES_DzS4: -0.018
+  # shear_calibration_parameters
+  DES_m1: 0.012
+  DES_m2: 0.012
+  DES_m3: 0.012
+  DES_m4: 0.012
+  # Intrinsic Alignment
+  DES_AIA: 1.0
+  DES_alphaIA: 1.0
+  DES_z0IA: 0.62
+"""
+
+test_params_clustering = """
+params:
+  # lens_photoz_errors
+  DES_DzL1: 0.002
+  DES_DzL2: 0.001
+  DES_DzL3: 0.003
+  DES_DzL4: 0
+  DES_DzL5: 0
+  # bin_bias
+  DES_b1: 1.45
+  DES_b2: 1.55
+  DES_b3: 1.65
+  DES_b4: 1.8
+  DES_b5: 2.0
+"""
+
+
+def test_cosmo_des_1yr_shear_camb(modules):
+    body_of_test(modules, "shear", "camb")
+
+
+def test_cosmo_des_1yr_ggl_camb(modules):
+    body_of_test(modules, "galaxy_galaxylensing", "camb")
+
+
+def test_cosmo_des_1yr_clustering_camb(modules):
+    body_of_test(modules, "clustering", "camb")
+
+
+def test_cosmo_des_1yr_shear_classy(modules):
+    body_of_test(modules, "shear", "classy")
+
+
+def test_cosmo_des_1yr_ggl_classy(modules):
+    body_of_test(modules, "galaxy_galaxylensing", "classy")
+
+
+def test_cosmo_des_1yr_clustering_classy(modules):
+    body_of_test(modules, "clustering", "classy")
+
+
+def body_of_test(modules, data, theory):
+    assert modules, "I need a modules folder!"
+    info = {_path_install: modules,
+            _theory: {theory: None},
+            _sampler: {"evaluate": None}}
+    info[_likelihood] = {"des_1yr_"+data: None}
+    info[_params] = {
+        _theory: {
+            "H0": 68.81,
+            "ombh2": 0.0468 * 0.6881 ** 2,
+            "omch2": (0.295 - 0.0468) * 0.6881 ** 2 - 0.0006155,
+            "YHe": 0.245341,
+            "tau": 0.08,
+            "As": 2.260574e-09,
+            "ns": 0.9676
+        }}
+    if data in ["shear", "galaxy_galaxylensing"]:
+        info[_params].update(yaml_load(test_params_shear)[_params])
+    if data in ["clustering", "galaxy_galaxylensing"]:
+        info[_params].update(yaml_load(test_params_clustering)[_params])
+
+
+    # UPDATE WITH BOTH ANYWAY FOR NOW!!!!!
+    info[_params].update(yaml_load(test_params_shear)[_params])
+    info[_params].update(yaml_load(test_params_clustering)[_params])
+    
+    
+    reference_value = 650.872548
+    abs_tolerance = 0.1
+    if theory == "classy":
+        info[_params][_theory].update(baseline_cosmology_classy_extra)
+        abs_tolerance += 2
+        print("WE SHOULD NOT HAVE TO LOWER THE TOLERANCE THAT MUCH!!!")
+    updated_info, products = run(info)
+    # print products["sample"]
+    computed_value = products["sample"]["chi2__"+list(info[_likelihood].keys())[0]].values[0]
+    assert (computed_value-reference_value) < abs_tolerance
