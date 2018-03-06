37de7d5f1ec6c8e534d521ae2929215be7cdd878
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-03-06 14:56:28 +0000

    input: oops

--- tests/test_input.py
+++ tests/test_input.py
@@ -14,15 +14,27 @@ from cobaya.yaml import yaml_load_file
 from cobaya.run import run
 from cobaya.log import HandledException
 
+# Aux definitions and functions
+
+test_info_common = {
+    _likelihood: {"_test": None},
+    _sampler: {"evaluate": None}}
+
+
+def _get_default_info(like_info, kind):
+    path_to_defaults = os.path.join(
+        get_folder(list(like_info.keys())[0], kind), _defaults_file)
+    return yaml_load_file(path_to_defaults)
+
 
 def test_prior_inherit_nonegiven():
-    updated_info, products = run(test_info)
-    default_info = _get_default_info(test_info[_likelihood], _likelihood)
+    updated_info, products = run(test_info_common)
+    default_info = _get_default_info(test_info_common[_likelihood], _likelihood)
     assert updated_info[_prior] == default_info[_prior]
 
 
 def test_prior_inherit_differentgiven():
-    test_info == deepcopy(test_info)
+    test_info = deepcopy(test_info_common)
     test_info[_prior] = {"third": "lambda a1: 1"}
     updated_info, products = run(test_info)
     default_info = _get_default_info(test_info[_likelihood], _likelihood)
@@ -31,7 +43,7 @@ def test_prior_inherit_differentgiven():
 
 
 def test_prior_inherit_samegiven():
-    test_info == deepcopy(test_info)
+    test_info = deepcopy(test_info_common)
     default_info = _get_default_info(test_info[_likelihood], _likelihood)
     name, prior = deepcopy(default_info[_prior]).popitem()
     test_info[_prior] = {name: prior}
@@ -40,7 +52,7 @@ def test_prior_inherit_samegiven():
 
 
 def test_prior_inherit_samegiven_differentdefinition():
-    test_info == deepcopy(test_info)
+    test_info = deepcopy(test_info_common)
     default_info = _get_default_info(test_info[_likelihood], _likelihood)
     name, prior = deepcopy(default_info[_prior]).popitem()
     test_info[_prior] = {name: "this is not a prior"}
@@ -49,7 +61,7 @@ def test_prior_inherit_samegiven_differentdefinition():
 
 
 def test_inherit_label_and_bounds():
-    test_info == deepcopy(test_info)
+    test_info = deepcopy(test_info_common)
     default_info_params = _get_default_info(test_info[_likelihood], _likelihood)[_params]
     test_info[_params] = deepcopy(default_info_params)
     test_info[_params]["a1"].pop(_p_label, None)
@@ -62,21 +74,10 @@ def test_inherit_label_and_bounds():
     assert updated_info[_params]["b1"].get("min") is None
     assert updated_info[_params]["b1"]["max"] == new_max
     # Second, remove limits, so they are inherited
+    test_info = deepcopy(test_info_common)
     test_info[_params] = deepcopy(default_info_params)
     test_info[_params]["b1"].pop("min")
     test_info[_params]["b1"].pop("max")
     updated_info, products = run(test_info)
     assert updated_info[_params]["b1"]["min"] == default_info_params["b1"]["min"]
     assert updated_info[_params]["b1"]["max"] == default_info_params["b1"]["max"]
-
-
-# Aux definitions and functions
-
-test_info = {
-    _likelihood: {"_test": None},
-    _sampler: {"evaluate": None}}
-
-
-def _get_default_info(like_info, kind):
-    path_to_defaults = os.path.join(get_folder(list(like_info.keys())[0], kind), _defaults_file)
-    return yaml_load_file(path_to_defaults)
