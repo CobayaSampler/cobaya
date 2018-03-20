8aa011761af0dd1578206ac5eef00e9d5c68affd
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-03-20 12:57:16 +0000

    py3: another attempt

--- tests/cosmo_install.py
+++ tests/cosmo_install.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
             "sn_jla_lite": None}}
     # Ignore Planck clik in Python 3
     if sys.version_info.major >= 3:
-        for lik in info_install[_likelihood]:
-            if lik.startswith("planck_2015") and not lik.endswith("cmblikes"):
-                info_install[_likelihood].pop(lik)
+        popliks = [lik for lik in info_install[_likelihood]
+                   if lik.startswith("planck_2015") and not lik.endswith("cmblikes")]
+        for lik in popliks:
+            info_install[_likelihood].pop(lik)
     path = sys.argv[1]
     import os
     if not os.path.exists(path):
