06d5617bb6c8f1d699ec40ff1d590ad75c08ce72
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-04-06 16:55:58 +0100

    hst: stupid error

--- tests/test_cosmo_hst.py
+++ tests/test_cosmo_hst.py
@@ -2,20 +2,22 @@ from __future__ import print_function
 import numpy as np
 from scipy.stats import norm
 
-from cobaya.conventions import _theory, _sampler, _likelihood, _params
+from cobaya.conventions import _theory, _sampler, _likelihood, _params, _path_install
 from cobaya.run import run
 
 
-def test_cosmo_hst_riess2018_camb():
-    body_of_test("riess2018", "camb")
+def test_cosmo_hst_riess2018_camb(modules):
+    body_of_test(modules, "riess2018", "camb")
 
 
-def test_cosmo_hst_riess2018_classy():
-    body_of_test("riess2018", "classy")
+def test_cosmo_hst_riess2018_classy(modules):
+    body_of_test(modules, "riess2018", "classy")
 
 
-def body_of_test(data, theory):
-    info = {_theory: {theory: None},
+def body_of_test(modules, data, theory):
+    assert modules, "I need a modules folder!"
+    info = {_path_install: modules,
+            _theory: {theory: None},
             _sampler: {"evaluate": None}}
     lik_name = "hst_"+data
     info[_likelihood] = {lik_name: None}
