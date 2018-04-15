98ff046e3f3019e0de01354d0a4a465bd670aa69
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-04-15 10:38:17 +0100

    docs: external links icon

--- docs/conf.py
+++ docs/conf.py
@@ -132,7 +132,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"logo_only": False, }
+html_theme_options = {"logo_only": False, "style_external_links": True}
 # For the readthedocs theme, check
 # https://github.com/rtfd/sphinx_rtd_theme/blob/master/sphinx_rtd_theme/theme.conf
 
