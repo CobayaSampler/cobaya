4996caa048f842abff45b815d8bb277d1ab80ecc
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-04-20 13:10:43 +0100

    version bump

--- cobaya/__init__.py
+++ cobaya/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Jesus Torrado and Antony Lewis"
-__version__ = "0.125"
-__year__ = "2017"
+__version__ = "0.126"
+__year__ = "2018"
 __url__ = "https://cobaya.readthedocs.io/en/latest/intro.html"
