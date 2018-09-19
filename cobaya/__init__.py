68bcf614593da3e009d23817596551db7acc8ae2
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-09-19 14:21:47 +0100

    version bump and pypi password

--- cobaya/__init__.py
+++ cobaya/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Jesus Torrado and Antony Lewis"
-__version__ = "1.0.1"
+__version__ = "1.0.3"
 __year__ = "2018"
-__url__ = "https://cobaya.readthedocs.io/en/latest/index.html"
+__url__ = "https://cobaya.readthedocs.io"
