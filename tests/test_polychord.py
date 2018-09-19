679b7fcc4eb1cf2e8395d27436f91282c9dbce14
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-09-19 10:44:55 +0100

    polychord: skipped speed test

--- tests/test_polychord.py
+++ tests/test_polychord.py
@@ -1,11 +1,10 @@
 from __future__ import division, print_function
 
+import pytest
 from flaky import flaky
 
 from common_sampler import body_of_test, body_of_test_speeds
 
-
-### import pytest
 ### @pytest.mark.mpi
 
 
@@ -22,11 +21,12 @@ def test_polychord(modules, tmpdir):
 def test_polychord_multimodal(modules, tmpdir):
     dimension = 2
     n_modes = 2
-    info_sampler = {"polychord": {"nlive": 25 * dimension * n_modes}}
+    info_sampler = {"polychord": {"nlive": 40 * dimension * n_modes}}
     body_of_test(dimension=dimension, n_modes=n_modes,
                  info_sampler=info_sampler, tmpdir=str(tmpdir), modules=modules)
 
 
+@pytest.skip
 @flaky(max_runs=2, min_passes=1)
 def test_polychord_speeds(modules):
     info_polychord = {"polychord": {}}
