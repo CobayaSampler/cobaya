97ce8bd70f4bda6c9fcc117a9028e1d2bf1273f4
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-10-11 14:10:38 +0200

    camb: specify halofit version

--- cobaya/theories/camb/camb.py
+++ cobaya/theories/camb/camb.py
@@ -322,6 +322,10 @@ class camb(_cosmo):
         # Generate and save
         self.log.debug("Setting parameters: %r", args)
         try:
+            # Halofit version is for now a global parameter
+            halofit_version = args.pop("halofit_version", None)
+            if halofit_version:
+                self.camb.set_halofit_version(version=halofit_version)
             cambparams = self.camb.set_params(**args)
             for attr, value in self.extra_attrs.items():
                 if hasattr(cambparams, attr):
