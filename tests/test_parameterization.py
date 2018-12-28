4c9eeb6fc859a120eaa4f89d3cc082fc2798d457
JesusTorrado <JesusTorrado@users.noreply.github.com>
2018-12-28 13:50:08 +0100

    parameterization: added test for fixing argument of function

--- tests/test_parameterization.py
+++ tests/test_parameterization.py
@@ -20,11 +20,12 @@ b_func = "lambda a, bprime: a+2*bprime"
 c_func = "lambda a, cprime: a+3*cprime"
 f_func = "lambda b: b**2"
 g_func = "lambda x: 3*x"
+h_func = "lambda i: i**2"
 
 
-def loglik(a, b, c, d, _derived=["x", "e"]):
+def loglik(a, b, c, d, h, i, _derived=["x", "e"]):
     _derived.update({"x": x_func(c), "e": e_func(b)})
-    return multivariate_normal.logpdf((a, b, c, d), cov=0.1 * np.eye(4))
+    return multivariate_normal.logpdf((a, b, c, d, h, i), cov=0.1 * np.eye(6))
 
 
 # Info
@@ -68,7 +69,10 @@ info = {
        # Dynamical derived parameter, needing non-mentioned output parameter (x)
        g:
          derived: "%s"
-    """ % (b_func, c_func, f_func, g_func))}
+       # Fixing parameter whose only role is being an argument for a different one
+       h: "%s"
+       i: 2
+    """ % (b_func, c_func, f_func, g_func, h_func))}
 
 
 def test_parameterization():
@@ -83,7 +87,9 @@ def test_parameterization():
         e = get_external_function(e_func)(b)
         f = get_external_function(f_func)(b)
         g = get_external_function(info[_params]["g"]["derived"])(x_func(point["c"]))
-        assert np.allclose(point[["b", "c", "e", "f", "g"]], [b, c, e, f, g])
+        h = get_external_function(info[_params]["h"])(info[_params]["i"])
+        assert np.allclose(point[["b", "c", "e", "f", "g", "h"]], [b, c, e, f, g, h])
+        # Test for GetDist too (except fixed ones, ignored by GetDist)
         bcefg_getdist = [gdsample.samples[i][gdsample.paramNames.list().index(p)]
-                         for p in ["b", "c", "e", "f", "g"]]
+                          for p in ["b", "c", "e", "f", "g"]]
         assert np.allclose(bcefg_getdist, [b, c, e, f, g])
