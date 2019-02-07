7a243898efcd56730f8f3428941b9be57118aa26
Antony Lewis <antony@cosmologist.info>
2019-02-07 09:52:54 +0000

    fix import_module path

--- tests/test_cosmo_docs_model.py
+++ tests/test_cosmo_docs_model.py
@@ -10,24 +10,20 @@ import sys
 import numpy as np
 from imageio import imread
 import pytest
+from six import StringIO
 
 from cobaya.conventions import _path_install
 from .install_for_tests import process_modules_path
 
 tests_folder = os.path.dirname(os.path.realpath(__file__))
-docs_folder = os.path.join(tests_folder, "../docs")
-docs_src_folder = os.path.join(docs_folder, "src_examples/cosmo_model/")
+docs_folder = os.path.join(tests_folder, "..", "docs")
+docs_src_folder = os.path.join(docs_folder, "src_examples", "cosmo_model")
 docs_img_folder = os.path.join(docs_folder, "img")
 
 # Number of possible different pixels
 pixel_tolerance = 0.995
 
 # Capture stdout in string
-if (sys.version_info > (3, 0)):
-    from io import StringIO
-else:
-    from StringIO import StringIO
-
 
 @contextmanager
 def stdout_redirector(stream):
@@ -46,12 +42,12 @@ def test_cosmo_docs_model(modules):
     cwd = os.getcwd()
     os.chdir(docs_src_folder)
     globals_example = {}
-    exec (open(os.path.join(docs_src_folder, "1.py")).read(), globals_example)
+    exec(open(os.path.join(docs_src_folder, "1.py")).read(), globals_example)
     globals_example["info"][_path_install] = modules
-    exec (open(os.path.join(docs_src_folder, "2.py")).read(), globals_example)
+    exec(open(os.path.join(docs_src_folder, "2.py")).read(), globals_example)
     stream = StringIO()
     with stdout_redirector(stream):
-        exec (open(os.path.join(docs_src_folder, "3.py")).read(), globals_example)
+        exec(open(os.path.join(docs_src_folder, "3.py")).read(), globals_example)
     # Comparing text output for this cell -- only derived parameter values
     out_filename = "3.out"
     derived_line_old, derived_line_new = map(
@@ -66,7 +62,7 @@ def test_cosmo_docs_model(modules):
     # Compare plots
     pre = "cosmo_model_"
     for filename, imgname in zip(["4.py", "5.py"], ["cltt.png", "omegacdm.png"]):
-        exec (open(os.path.join(docs_src_folder, filename)).read(), globals_example)
+        exec(open(os.path.join(docs_src_folder, filename)).read(), globals_example)
         old_img = imread(os.path.join(docs_img_folder, pre + imgname)).astype(float)
         new_img = imread(imgname).astype(float)
         npixels = (lambda x: x.shape[0] + x.shape[1])(old_img)
