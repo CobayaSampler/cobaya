f1e355a6f21a4029e896f4cf83c80792ca4cf625
torradocacho <JesusTorrado@users.noreply.github.com>
2019-06-12 12:24:11 +0200

    evaluate: allow for >1 samples

--- cobaya/samplers/evaluate/evaluate.py
+++ cobaya/samplers/evaluate/evaluate.py
@@ -26,7 +26,14 @@ class evaluate(Sampler):
         Creates a 1-point collection to store the point
         at which the posterior is evaluated.
         """
-        self.one_point = Collection(self.model, self.output, initial_size=1, name="1")
+        try:
+            self.N = int(self.N)
+        except:
+            self.log.error(
+                "Could not convert the number of samples to an integer: %r", self.N)
+            raise HandledException
+        self.one_point = Collection(
+            self.model, self.output, initial_size=self.N, name="1")
         self.log.info("Initialized!")
 
     def run(self):
@@ -38,40 +45,45 @@ class evaluate(Sampler):
         Then it evaluates the prior and likelihood(s) and stores them in the one-member
         sample collection.
         """
-        self.log.info("Looking for a reference point with non-zero prior.")
-        reference_point = self.model.prior.reference()
-        reference_point = odict(
-            zip(self.model.parameterization.sampled_params(), reference_point))
-        for p, v in (self.override or {}).items():
-            if p not in reference_point:
-                self.log.error("Parameter '%s' used in override not known. "
-                               "Known parameters names are %r.",
-                               p, self.model.parameterization.sampled_params())
-                raise HandledException
-            reference_point[p] = v
-        self.log.info("Reference point:\n   " + "\n   ".join(
-            ["%s = %g" % pv for pv in reference_point.items()]))
-        self.log.info("Evaluating prior and likelihoods...")
-        self.logposterior = self.model.logposterior(reference_point)
-        self.one_point.add(
-            list(reference_point.values()), derived=self.logposterior.derived,
-            logpost=self.logposterior.logpost, logpriors=self.logposterior.logpriors,
-            loglikes=self.logposterior.loglikes)
-        self.log.info("log-posterior  = %g", self.logposterior.logpost)
-        self.log.info("log-prior      = %g", sum(self.logposterior.logpriors))
-        for i, name in enumerate(self.model.prior):
-            self.log.info("   logprior_" + name + " = %g" % self.logposterior.logpriors[i])
-        if sum(self.logposterior.logpriors) > -np.inf:
-            self.log.info("log-likelihood = %g", sum(self.logposterior.loglikes))
-            for i, name in enumerate(self.model.likelihood):
-                self.log.info("   chi2_" + name + " = %g" % (-2 * self.logposterior.loglikes[i]))
-            self.log.info("Derived params:")
-            for name, value in zip(self.model.parameterization.derived_params(),
-                                   self.logposterior.derived):
-                self.log.info("   " + name + " = %g" % value)
-        else:
-            self.log.info("Likelihoods and derived parameters not computed, "
-                          "since the prior is null.")
+        for i in range(self.N):
+            if self.N > 1:
+                self.log.info("Evaluating sample #%d ------------------------------", i+1)
+            self.log.info("Looking for a reference point with non-zero prior.")
+            reference_point = self.model.prior.reference()
+            reference_point = odict(
+                zip(self.model.parameterization.sampled_params(), reference_point))
+            for p, v in (self.override or {}).items():
+                if p not in reference_point:
+                    self.log.error("Parameter '%s' used in override not known. "
+                                   "Known parameters names are %r.",
+                                   p, self.model.parameterization.sampled_params())
+                    raise HandledException
+                reference_point[p] = v
+            self.log.info("Reference point:\n   " + "\n   ".join(
+                ["%s = %g" % pv for pv in reference_point.items()]))
+            self.log.info("Evaluating prior and likelihoods...")
+            self.logposterior = self.model.logposterior(reference_point)
+            self.one_point.add(
+                list(reference_point.values()), derived=self.logposterior.derived,
+                logpost=self.logposterior.logpost, logpriors=self.logposterior.logpriors,
+                loglikes=self.logposterior.loglikes)
+            self.log.info("log-posterior  = %g", self.logposterior.logpost)
+            self.log.info("log-prior      = %g", sum(self.logposterior.logpriors))
+            for i, name in enumerate(self.model.prior):
+                self.log.info(
+                    "   logprior_" + name + " = %g", self.logposterior.logpriors[i])
+            if sum(self.logposterior.logpriors) > -np.inf:
+                self.log.info("log-likelihood = %g", sum(self.logposterior.loglikes))
+                for i, name in enumerate(self.model.likelihood):
+                    self.log.info(
+                        "   chi2_" + name + " = %g", (-2 * self.logposterior.loglikes[i]))
+                self.log.info("Derived params:")
+                for name, value in zip(self.model.parameterization.derived_params(),
+                                       self.logposterior.derived):
+                    self.log.info("   " + name + " = %g", value)
+            else:
+                self.log.info("Likelihoods and derived parameters not computed, "
+                              "since the prior is null.")
 
     def close(self, exception_type, exception_value, traceback):
         """
