19e4a9b42cd26f7cbd719565151bc5a1ca3bf27d
torradocacho <JesusTorrado@users.noreply.github.com>
2019-06-19 14:36:22 +0200

    minimize: added diagonal rescaling (no rotation yet)

--- cobaya/samplers/minimize/minimize.py
+++ cobaya/samplers/minimize/minimize.py
@@ -24,10 +24,10 @@ import logging
 
 # Local
 from cobaya.sampler import Sampler
-from cobaya.mpi import get_mpi_rank, get_mpi_size, get_mpi_comm
+from cobaya.mpi import get_mpi_size, get_mpi_comm, am_single_or_primary_process
 from cobaya.collection import OnePoint
 from cobaya.log import HandledException
-from cobaya.tools import read_dnumber
+from cobaya.tools import read_dnumber, choleskyL
 
 
 # Handling scpiy vs BOBYQA
@@ -37,14 +37,15 @@ evals_attr = {"scipy": "fun", "bobyqa": "f"}
 class minimize(Sampler):
     def initialize(self):
         """Prepares the arguments for `scipy.minimize`."""
-        if not get_mpi_rank():
+        if am_single_or_primary_process():
             self.log.info("Initializing")
         self.max_evals = read_dnumber(self.max_evals, self.model.prior.d())
         # Configure target
-        self.logp = (
-            (lambda x: self.model.logposterior(x, make_finite=True)[0])
-            if not self.ignore_prior else
-            (lambda x: sum(self.model.loglikes(x, return_derived=True)[0])))
+        method = self.model.loglike if self.ignore_prior else self.model.logpost
+        kwargs = {"make_finite": True}
+        if self.ignore_prior:
+            kwargs.update({"return_derived": False})
+        self.logp = lambda x: method(x, **kwargs)
         # Try to load info from previous samples.
         # If none, sample from reference (make sure that it has finite like/post)
         initial_point = None
@@ -58,6 +59,7 @@ class minimize(Sampler):
                     list(self.model.parameterization.sampled_params())].values
                 self.log.info("Starting from %s of previous chain:",
                               "best fit" if self.ignore_prior else "MAP")
+                covmat = collection_in.cov()
         if initial_point is None:
             this_logp = -np.inf
             while not np.isfinite(this_logp):
@@ -65,23 +67,39 @@ class minimize(Sampler):
                 this_logp = self.logp(initial_point)
             self.log.info("Starting from random initial point:")
         self.log.info(dict(zip(self.model.parameterization.sampled_params(), initial_point)))
-        self.linear_transform = np.eye(self.model.prior.d())
-        # Configure transformation for target
-        self.logp_transf = lambda x: self.logp(self.transform(x))
+        # Cov and affine transformation
+        covmat = None
+        self._affine_transform_matrix = None
+        self._inv_affine_transform_matrix = None
+        self._affine_transform_baseline = None
+        if covmat is not None:
+            sigmas_diag, L = choleskyL(covmat, return_scale_free=True)
+            self._affine_transform_matrix = np.linalg.inv(sigmas_diag)
+            self._inv_affine_transform_matrix = sigmas_diag
+            self._affine_transform_baseline = initial_point
+            # Transforms to space where initial point is at centre, and cov is normalised
+            self.affine_transform = lambda x: (
+                self._affine_transform_matrix.dot(x - self._affine_transform_baseline))
+            self.inv_affine_transform = lambda x: (
+                self._inv_affine_transform_matrix.dot(x) + self._affine_transform_baseline)
+        else:
+            self.affine_transform = lambda x: x
+            self.inv_affine_transform = lambda x: x
+        bounds = self.model.prior.bounds(
+            confidence_for_unbounded=self.confidence_for_unbounded)
+        # Re-scale
+        self.logp_transf = lambda x: self.logp(self.inv_affine_transform(x))
+        initial_point = self.affine_transform(initial_point)
+        bounds = self.affine_transform(bounds)
         # Configure method
         if self.method.lower() == "bobyqa":
             self.minimizer = pybobyqa.solve
             self.kwargs = {
                 "objfun": (lambda x: -self.logp_transf(x)),
                 "x0": initial_point,
-                "bounds": np.array(list(
-                    zip(*self.model.prior.bounds(
-                        confidence_for_unbounded=self.confidence_for_unbounded)))),
+                "bounds": np.array(list(zip(*bounds))),
                 "seek_global_minimum": True,
-                "scaling_within_bounds": hasattr(self, "covmat"),
-
-                "maxfun": int(self.max_evals),
-                }
+                "maxfun": int(self.max_evals)}
             self.kwargs.update(self.override or {})
             self.log.debug("Arguments for pybobyqa.solve:\n%r",
                            {k:v for k,v in self.kwargs.items() if k != "objfun"})
@@ -90,8 +108,7 @@ class minimize(Sampler):
             self.kwargs = {
                 "fun": (lambda x: -self.logp_transf(x)),
                 "x0": initial_point,
-                "bounds": self.model.prior.bounds(
-                    confidence_for_unbounded=self.confidence_for_unbounded),
+                "bounds": bounds,
                 "tol": self.tol,
                 "options": {
                     "maxiter": self.max_evals,
@@ -104,9 +121,6 @@ class minimize(Sampler):
             self.log.error(
                 "Method '%s' not recognized. Try one of %r.", self.method, methods)
             raise HandledException
-
-    def transform(self, point):
-        return(self.linear_transform.dot(point))
 
     def run(self):
         """
@@ -157,30 +171,32 @@ class minimize(Sampler):
             return
         if get_mpi_size():
             results = get_mpi_comm().gather(self.result, root=0)
-            if not get_mpi_rank():
+            if am_single_or_primary_process():
                 self.result = results[np.argmin(
                     [getattr(r, evals_attr_) for r in results])]
-        if not get_mpi_rank():
+        if am_single_or_primary_process():
             if not self.success:
-                self.log.error("Maximization failed! Here is the `scipy` raw result:\n%r",
+                self.log.error("Minimization failed! Here is the `scipy` raw result:\n%r",
                                self.result)
                 raise HandledException
             logpost = -np.array(getattr(self.result, evals_attr_))
+            x_minimum = self.inv_affine_transform(self.result.x)
             self.log.info("log%s maximized at %g",
                           "likelihood" if self.ignore_prior else "posterior", logpost)
-            post = self.model.logposterior(self.result.x)
+            post = self.model.logposterior(x_minimum)
             recomputed_max = sum(post.loglikes) if self.ignore_prior else post.logpost
             if not np.allclose(logpost, recomputed_max):
                 self.log.error("Cannot reproduce result. Something bad happened. "
-                               "Recomputed max: %g at %r", recomputed_max, self.result.x)
+                               "Recomputed max: %g at %r", recomputed_max, x_minimum)
                 raise HandledException
-            self.maximum = OnePoint(
+            self.minimum = OnePoint(
                 self.model, self.output, name="maximum",
                 extension=("likelihood" if self.ignore_prior else "posterior"))
-            self.maximum.add(self.result.x, derived=post.derived, logpost=post.logpost,
+            self.minimum.add(x_minimum, derived=post.derived, logpost=post.logpost,
                              logpriors=post.logpriors, loglikes=post.loglikes)
-            self.log.info("Parameter values at maximum:\n%s"%self.maximum.data.to_string())
-            self.maximum._out_update()
+            self.log.info(
+                "Parameter values at maximum:\n%s", self.minimum.data.to_string())
+            self.minimum._out_update()
 
     def products(self):
         """
@@ -188,9 +204,17 @@ class minimize(Sampler):
 
         Returns:
            The :class:`OnePoint` that maximizes the posterior or likelihood (depending on
-           ``ignore_prior``), and the `scipy.optimize.OptimizeResult
+           ``ignore_prior``), the ``result_object`` instance of `scipy
            <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html>`_
-           instance.
+           or `pyBOBYQA
+           <https://numericalalgorithmsgroup.github.io/pybobyqa/build/html/userguide.html>`_,
+           and the inverse of the affine transform under which the minimizer has worked, 
+           as a matrix ``M`` and a point ``X0``, from which the real space points can be
+           obtained as :math:`x = M x^\prime + X0`, where :math:`x^\prime` represents the
+           parameter space used in ``result_object`` (returns ``None`` for both ``M`` and
+           ``X0`` if no transformation was applied).
         """
-        if not get_mpi_rank():
-            return {"maximum": self.maximum, "result_object": self.result}
+        if am_single_or_primary_process():
+            return {"minimum": self.minimum, "result_object": self.result,
+                    "M": self._inv_affine_transform_matrix,
+                    "X0": self._affine_transform_baseline}
