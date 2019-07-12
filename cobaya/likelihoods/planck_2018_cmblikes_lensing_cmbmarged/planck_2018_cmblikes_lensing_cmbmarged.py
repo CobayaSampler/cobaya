80d19c711c65a64b2dc771e70d734b27b4c41756
torradocacho <JesusTorrado@users.noreply.github.com>
2019-07-12 08:54:32 +0200

    planck: updated supp covmats in rest of likelihoods

--- cobaya/likelihoods/planck_2018_cmblikes_lensing_cmbmarged/planck_2018_cmblikes_lensing_cmbmarged.py
+++ cobaya/likelihoods/planck_2018_cmblikes_lensing_cmbmarged/planck_2018_cmblikes_lensing_cmbmarged.py
@@ -25,7 +25,7 @@ class planck_2018_cmblikes_lensing_cmbmarged(_cmblikes_prototype):
 
 # name of the supplementary data and covmats repo/folder
 supp_data_name = "planck_supp_data_and_covmats"
-supp_data_version = "v1.0"
+supp_data_version = "v2.0"
 
 
 def get_path(path):
