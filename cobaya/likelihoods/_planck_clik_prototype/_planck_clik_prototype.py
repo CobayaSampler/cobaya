2c9a19ff98fe8b1244064e4be1758de3a11c7a27
Antony Lewis <antony@cosmologist.info>
2019-07-16 10:22:51 +0100

    cfitsio link patch

--- cobaya/likelihoods/_planck_clik_prototype/_planck_clik_prototype.py
+++ cobaya/likelihoods/_planck_clik_prototype/_planck_clik_prototype.py
@@ -381,18 +381,18 @@ def install_clik(path, no_progress_bars=False):
     log.info('Installing from directory %s' % source_dir)
     # The following code patches a problem with the download source of cfitsio.
     # Left here in case the FTP server breaks again.
-    # if True:  # should be fixed: maybe a ping to the FTP server???
-    #     log.info("Patching origin of cfitsio")
-    #     cfitsio_filename = os.path.join(source_dir, "waf_tools", "cfitsio.py")
-    #     with open(cfitsio_filename, "r") as cfitsio_file:
-    #         lines = cfitsio_file.readlines()
-    #         i_offending = next(i for i, l in enumerate(lines) if ".tar.gz" in l)
-    #         lines[i_offending] = (
-    #             "  atl.installsmthg_pre(ctx,"
-    #             "'http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3280.tar.gz',"
-    #             "'cfitsio3280.tar.gz')\n")
-    #     with open(cfitsio_filename, "w") as cfitsio_file:
-    #         cfitsio_file.write("".join(lines))
+    if True:  # should be fixed: maybe a ping to the FTP server???
+        log.info("Patching origin of cfitsio")
+        cfitsio_filename = os.path.join(source_dir, "waf_tools", "cfitsio.py")
+        with open(cfitsio_filename, "r") as cfitsio_file:
+            lines = cfitsio_file.readlines()
+            i_offending = next(i for i, l in enumerate(lines) if ".tar.gz" in l)
+            lines[i_offending] = (
+                "  atl.installsmthg_pre(ctx,"
+                "'https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3280.tar.gz',"
+                "'cfitsio3280.tar.gz')\n")
+        with open(cfitsio_filename, "w") as cfitsio_file:
+            cfitsio_file.write("".join(lines))
     cwd = os.getcwd()
     try:
         os.chdir(source_dir)
