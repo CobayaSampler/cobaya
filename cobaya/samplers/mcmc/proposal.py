37091f2dd710d3c8b65f45330b06590e5cb7d023
Antony Lewis <antony@cosmologist.info>
2019-07-17 12:00:31 +0100

    fix bug in _cosmo, unresolved 'log'

--- cobaya/samplers/mcmc/proposal.py
+++ cobaya/samplers/mcmc/proposal.py
@@ -105,12 +105,12 @@ class BlockedProposer(HasLogger):
         else:
             if len(oversampling_factors) != len(parameter_blocks):
                 raise LoggedError(
-                    log, "List of oversampling factors has a different length that "
-                    "list of blocks: %d vs %d.",
+                    self.log, "List of oversampling factors has a different length that "
+                              "list of blocks: %d vs %d.",
                     len(oversampling_factors), len(parameter_blocks))
             if np.any(oversampling_factors != np.floor(np.array(oversampling_factors))):
                 raise LoggedError(
-                    log, "Oversampling factors must be integer! Got %r.",
+                    self.log, "Oversampling factors must be integer! Got %r.",
                     oversampling_factors)
             self.oversampling_factors = np.array(oversampling_factors, dtype=int)
         # Turn it into per-block: multiply by number of params in block
@@ -120,13 +120,13 @@ class BlockedProposer(HasLogger):
         else:
             if i_last_slow_block > len(parameter_blocks) - 1:
                 raise LoggedError(
-                    log, "The index given for the last slow block, %d, is not valid: "
-                    "there are only %d blocks.",
+                    self.log, "The index given for the last slow block, %d, is not valid: "
+                              "there are only %d blocks.",
                     i_last_slow_block, len(parameter_blocks))
         n_all = sum([len(b) for b in parameter_blocks])
         n_slow = sum([len(b) for b in parameter_blocks[:1 + i_last_slow_block]])
         if set(list(chain(*parameter_blocks))) != set(range(n_all)):
-            raise LoggedError(log, "The blocks do not contain all the parameter indices.")
+            raise LoggedError(self.log, "The blocks do not contain all the parameter indices.")
         # Mapping between internal indices, sampler parameter indices and blocks:
         # let i=0,1,... be the indices of the parameters for the sampler,
         # and j=0,1,... be the indices of the parameters *as given to the proposal*
@@ -188,12 +188,12 @@ class BlockedProposer(HasLogger):
         """
         if propose_matrix.shape[0] != self.d():
             raise LoggedError(
-                log, "The covariance matrix does not have the correct dimension: "
-                "it's %d, but it should be %d.", propose_matrix.shape[0], self.d())
+                self.log, "The covariance matrix does not have the correct dimension: "
+                     "it's %d, but it should be %d.", propose_matrix.shape[0], self.d())
         if not (np.allclose(propose_matrix.T, propose_matrix) and
                 np.all(np.linalg.eigvals(propose_matrix) > 0)):
-            raise LoggedError(log, "The given covmat is not a positive-definite, "
-                              "symmetric square matrix.")
+            raise LoggedError(self.log, "The given covmat is not a positive-definite, "
+                                   "symmetric square matrix.")
         self.propose_matrix = propose_matrix.copy()
         propose_matrix_j_sorted = self.propose_matrix[np.ix_(self.i_of_j, self.i_of_j)]
         sigmas_diag, L = choleskyL(propose_matrix_j_sorted, return_scale_free=True)
