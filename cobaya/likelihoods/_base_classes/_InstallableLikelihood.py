efa95ca1c7d276b5b7cedb680c7c425244e56b59
Antony Lewis <antony@cosmologist.info>
2019-11-10 11:28:45 +0000

    remove cobaya github from _InstallableLikelihood base

--- cobaya/likelihoods/_base_classes/_InstallableLikelihood.py
+++ cobaya/likelihoods/_base_classes/_InstallableLikelihood.py
@@ -12,12 +12,10 @@ import logging
 
 # Local
 from cobaya.likelihood import Likelihood
-from cobaya.likelihoods._base_classes._planck_clik_prototype import last_version_supp_data_and_covmats
 
 
 class _InstallableLikelihood(Likelihood):
-    install_options = {"github_repository": "CobayaSampler/planck_supp_data_and_covmats",
-                       "github_release": last_version_supp_data_and_covmats}
+    install_options = {}
 
     @classmethod
     def get_install_options(cls):
