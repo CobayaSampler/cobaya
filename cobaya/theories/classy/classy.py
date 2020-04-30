ab5abb3b1c2ae0a9575c30ecaf45635b73f5ebcf
torradocacho <JesusTorrado@users.noreply.github.com>
2020-04-30 16:29:31 +0200

    classy: version update

--- cobaya/theories/classy/classy.py
+++ cobaya/theories/classy/classy.py
@@ -164,7 +164,7 @@ non_linear_default_code = "hmcode"
 class classy(BoltzmannBase):
     # Name of the Class repo/folder and version to download
     _classy_repo_name = "lesgourg/class_public"
-    _min_classy_version = "v2.8.2"
+    _min_classy_version = "v2.9.3"
     _classy_repo_version = os.environ.get('CLASSY_REPO_VERSION', _min_classy_version)
 
     def initialize(self):
