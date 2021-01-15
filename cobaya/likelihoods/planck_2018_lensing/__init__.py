769774d4ba7e760227caf3d6a4dba151555eccbe
torradocacho <JesusTorrado@users.noreply.github.com>
2021-01-15 18:04:32 +0100

    bib: des + planck lensing to new format

--- cobaya/likelihoods/planck_2018_lensing/__init__.py
+++ cobaya/likelihoods/planck_2018_lensing/__init__.py
@@ -4,6 +4,11 @@ from cobaya.likelihoods._base_classes._planck_clik_prototype import \
 
 
 class native(_CMBlikes):
+    r"""
+    Lensing likelihood of Planck's 2018 data release based on temperature+polarization
+    map-based lensing reconstruction \cite{Aghanim:2018oex} (native Python
+    re-implementation by A.~Lewis).
+    """
     install_options = {"github_repository": "CobayaSampler/planck_supp_data_and_covmats",
                        "github_release": last_version_supp_data_and_covmats}
 
@@ -11,4 +16,9 @@ class native(_CMBlikes):
 
 
 class CMBMarged(native):
+    r"""
+    Lensing likelihood of Planck's 2018 data release based on temperature+polarization
+    map-based lensing reconstruction, marginalized over the CMB power spectra
+    \cite{Aghanim:2018oex} (native Python re-implementation by A.~Lewis).
+    """
     pass
