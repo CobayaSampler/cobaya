8fe36cb84cc1956267f8528046ec91584ff84a26
Erik Rosenberg <errosenberg47@gmail.com>
2021-01-15 23:18:40 +0000

    Remove 'fg' and 'both' files
    Currently only releasing the CamSpec12.5cl data. That should be run with the "normal" branch.
    Therefore removing 'fg' and 'both' py and yaml files for simplicity.

--- cobaya/likelihoods/planck_2020_highl_CamSpec/TTTEEE_fg.py
+++
@@ -1,4 +0,0 @@
-from cobaya.likelihoods.planck_2020_highl_CamSpec.planck_2020_CamSpec_python import planck_2020_CamSpec_python
-
-class TTTEEE_CS2020_fg(planck_2020_CamSpec_python):
-    pass
--- cobaya/likelihoods/planck_2020_highl_CamSpec/TT_fg.py
+++
@@ -1,4 +0,0 @@
-from cobaya.likelihoods.planck_2020_highl_CamSpec.planck_2020_CamSpec_python import planck_2020_CamSpec_python
-
-class TT_CS2020_fg(planck_2020_CamSpec_python):
-    pass
