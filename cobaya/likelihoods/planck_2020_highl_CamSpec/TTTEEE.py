200bd94038ce77a86f6ff0b0625baf0605b25be3
Erik Rosenberg <errosenberg47@gmail.com>
2021-01-17 12:37:48 +0000

    Add description and citation to likelihood classes

--- cobaya/likelihoods/planck_2020_highl_CamSpec/TTTEEE.py
+++ cobaya/likelihoods/planck_2020_highl_CamSpec/TTTEEE.py
@@ -1,4 +1,8 @@
 from cobaya.likelihoods.planck_2020_highl_CamSpec.planck_2020_CamSpec_python import planck_2020_CamSpec_python
 
 class TTTEEE(planck_2020_CamSpec_python):
+    r"""
+    High-$\ell$ temperature and polarization \textsc{CamSpec} 12.5HMcl likelihood Planck 2018 data
+    \cite{efstathiou2019}.
+    """
     pass
