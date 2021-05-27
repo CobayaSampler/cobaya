319cd03a3ab6ed22e5bb1eeb40551e4d13dcc69b
Erik Rosenberg <errosenberg47@gmail.com>
2021-05-27 19:31:40 +0100

    Fix import problem; add path name to module

--- cobaya/likelihoods/planck_2018_highl_CamSpec2021/TEEE.py
+++ cobaya/likelihoods/planck_2018_highl_CamSpec2021/TEEE.py
@@ -1,4 +1,4 @@
-from planck_2018_highl_CamSpec2021_python import Planck2018CamSpec2021Python
+from cobaya.likelihoods.planck_2018_highl_CamSpec2021.planck_2018_CamSpec2021_python import Planck2018CamSpec2021Python
 
 class TEEE(Planck2018CamSpec2021Python):
     r"""
