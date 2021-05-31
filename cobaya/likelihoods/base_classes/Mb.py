d27718999caf46767c3a2ea53a4895e060846b65
Antony Lewis <cmbant@users.noreply.github.com>
2021-05-31 13:15:26 +0100

    Local Hubble module in terms of magnitude (#179)
    * Added absolute magnitude to SNe module
    
    * Eliminated Mb from yaml parameter block
    
    * Working Mb likelihood
    
    * Small fixes
    
    * Added test
    
    * Updated input_database
    
    * Added warning
    
    * tidy and rebase to latest master
    
    * add test from other branch
    
    Co-authored-by: Pablo Lemos <plemos91@gmail.com>

---
+++ cobaya/likelihoods/base_classes/Mb.py
@@ -0,0 +1,28 @@
+r"""
+.. module:: Mb
+
+:Synopsis: Prototype class for local Hubble parameter measurements quantified in terms
+    of the magnitude measurement (closer to what is measured than H0)
+:Author: Pablo Lemos
+"""
+
+from cobaya.likelihood import Likelihood
+
+
+class Mb(Likelihood):
+    # Data type for aggregated chi2 (case sensitive)
+    type = "Mb"
+
+    # variables from yaml
+    Mb_mean: float
+    Mb_std: float
+
+    def initialize(self):
+        self.minus_half_invvar = - 0.5 / self.Mb_std ** 2
+
+    def get_requirements(self):
+        return {}
+
+    def logp(self, **params_values):
+        Mb_theory = params_values.get("Mb", None)
+        return self.minus_half_invvar * (Mb_theory - self.Mb_mean) ** 2
