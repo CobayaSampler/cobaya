ece1d73b6159013c92ab54fe26762a1eb37d4625
Pablo Lemos <plemos91@gmail.com>
2021-06-11 16:43:37 +0100

    Added data files for LYxQSO

---
+++ cobaya/likelihoods/bao/sdss_dr16_baoplus_lyxqso.py
@@ -0,0 +1,9 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_dr16_baoplus_lyxqso(BAO):
+    r"""
+    Likelihood of the power spectrum Lyman alpha x QSO BAO of SDSS DR16 
+    \cite{Alam:2020sor}.
+    """
+    pass
