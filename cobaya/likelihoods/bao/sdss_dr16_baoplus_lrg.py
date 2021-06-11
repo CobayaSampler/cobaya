9194ac9fd03484530167c682e16c9c8fdd0c8f62
Pablo Lemos <plemos91@gmail.com>
2021-06-11 14:33:53 +0100

    Added eBOSS DR16 LRG likelihood)

---
+++ cobaya/likelihoods/bao/sdss_dr16_baoplus_lrg.py
@@ -0,0 +1,8 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_dr16_baoplus_lrg(BAO):
+    r"""
+    Likelihood of the power spectrum LRG BAO of SDSS DR16 \cite{Alam:2020sor}.
+    """
+    pass
