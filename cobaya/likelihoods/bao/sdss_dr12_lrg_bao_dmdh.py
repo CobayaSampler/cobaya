54f76af3d58af7981f25e7c0b6b015f96ba1efa4
Mark Mirmelstein <markm@cori10.nersc.gov>
2022-02-15 08:55:56 -0800

    fixed bib for DR12

--- cobaya/likelihoods/bao/sdss_dr12_lrg_bao_dmdh.py
+++ cobaya/likelihoods/bao/sdss_dr12_lrg_bao_dmdh.py
@@ -3,6 +3,6 @@ from cobaya.likelihoods.base_classes import BAO
 
 class sdss_dr12_lrg_bao_dmdh(BAO):
     r"""
-    Likelihood of the power spectrum LRG BAO of SDSS DR12 \cite{Alam:2020sor}.
+    Likelihood of the power spectrum LRG BAO of SDSS DR12 \cite{Alam:2016hwk}.
     """
     pass
