2c87b18e9513efc1f746f1c6f82840d662213656
Mark Mirmelstein <markm@cori10.nersc.gov>
2022-02-15 05:12:03 -0800

    added BAO DR12 and DR16 LRG likelihoods

---
+++ cobaya/likelihoods/bao/sdss_dr12_lrg_bao_dmdh.py
@@ -0,0 +1,8 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_dr12_lrg_bao_dmdh(BAO):
+    r"""
+    Likelihood of the power spectrum LRG BAO of SDSS DR12 \cite{Alam:2020sor}.
+    """
+    pass
