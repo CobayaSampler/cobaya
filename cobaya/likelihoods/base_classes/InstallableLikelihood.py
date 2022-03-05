0957df753774d396656c5e4db02a45cb9199689f
torradocacho <JesusTorrado@users.noreply.github.com>
2022-03-05 11:45:14 +0100

    InstallableLikelihood: better path handling [skip travis]

--- cobaya/likelihoods/base_classes/InstallableLikelihood.py
+++ cobaya/likelihoods/base_classes/InstallableLikelihood.py
@@ -1,7 +1,7 @@
 r"""
 .. module:: InstallableLikelihood
 
-:Synopsis: Prototype class adding class methods for simple installation of likelihood data.
+:Synopsis: Prototype class with class methods for simple installation of likelihood data.
 :Author: Jesus Torrado and Antony Lewis
 
 """
@@ -19,26 +19,50 @@ from cobaya.tools import VersionCheckError
 
 
 class InstallableLikelihood(Likelihood):
+    """
+    Prototype class for likelihood with installation and installation-check methods.
+    """
+
     install_options: InfoDict = {}
 
     @classmethod
     def get_install_options(cls):
+        """
+        Returns class variables containing e.g. a download url, a version tag...
+        """
         return cls.install_options
 
     @classmethod
     def get_path(cls, path):
+        """
+        Returns the (real) path where the package will be installed,
+        given a global installation ``path``.
+
+        Repeated recursive calls on the same global path produce the same output,
+        i.e. ``cls.get_path(cls.get_path(path)) = cls.get_path(path)``.
+        """
         opts = cls.get_install_options()
         repo = opts.get("github_repository", None)
         if repo:
             data_path = repo.split('/')[-1]
         else:
             data_path = opts.get("data_path", cls.__name__)
-        return os.path.realpath(os.path.join(path, "data", data_path))
+        install_path = os.path.realpath(os.path.join(path, "data", data_path))
+        # Idempotent: return if input == proposed output
+        if path.rstrip(os.sep).endswith(data_path.rstrip(os.sep)):
+            return path
+        return install_path
 
     @classmethod
     def is_installed(cls, **kwargs):
+        """
+        Performs an installation check and returns ``True`` if successful, ``False`` if
+        not, or raises `:class:`tools.VersionCheckError` if there is an obsolete
+        installation.
+        """
         if kwargs.get("data", True):
             path = kwargs["path"]
+            path = cls.get_path(path)  # ensure full install path passed
             opts = cls.get_install_options()
             if not opts:
                 return True
@@ -62,6 +86,9 @@ class InstallableLikelihood(Likelihood):
 
     @classmethod
     def install(cls, path=None, data=True, no_progress_bars=False, **_kwargs):
+        """
+        Installs the necessary data packages for this likelihood.
+        """
         if not data:
             return True
         log = get_logger(cls.get_qualified_class_name())
