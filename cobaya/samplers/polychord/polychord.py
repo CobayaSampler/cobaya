6fee41145846785a31176a2040e127ed553a2025
torradocacho <JesusTorrado@users.noreply.github.com>
2022-03-05 23:08:11 +0100

    polychord: some fixes [skip travis]

--- cobaya/samplers/polychord/polychord.py
+++ cobaya/samplers/polychord/polychord.py
@@ -144,7 +144,7 @@ class polychord(Sampler):
                    "cluster_posteriors", "write_resume", "read_resume",
                    "write_stats", "write_live", "write_dead", "write_prior",
                    "maximise", "compression_factor", "synchronous", "base_dir",
-                   "file_root", "seed", "grade_dims", "grade_frac", "nlives"]
+                   "file_root", "grade_dims", "grade_frac", "nlives"]
         # As stated above, num_repeats is ignored, so let's not pass it
         pc_args.pop(pc_args.index("num_repeats"))
         settings: Any = load_module('pypolychord.settings', path=self._poly_build_path,
