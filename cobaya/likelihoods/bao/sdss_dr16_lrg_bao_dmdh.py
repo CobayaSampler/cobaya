88cd22f0b7e7fda7212d6616aef94fb27b4ea8b4
Mark Mirmelstein <markm42@users.noreply.github.com>
2022-09-26 10:57:29 +0100

    added files for the BAO DR12 and DR16 LRG likelihoods (#235)
    * added BAO DR12 and DR16 LRG likelihoods
    
    * fixed bib for DR12
    
    Co-authored-by: Antony Lewis <cmbant@users.noreply.github.com>

---
+++ cobaya/likelihoods/bao/sdss_dr16_lrg_bao_dmdh.py
@@ -0,0 +1,9 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_dr16_lrg_bao_dmdh(BAO):
+    r"""
+    Likelihood of the power spectrum LRG BAO of SDSS DR16 \cite{Alam:2020sor}.
+    It does not include the DR12 measurements.
+    """
+    pass
