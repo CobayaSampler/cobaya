21173a0067b2463b7141944c7e2a5b1d2ff366d4
Minh Nguyen <nhat.minh.nguyen.111@gmail.com>
2023-03-24 23:23:58 -0400

    Add derived Lya likelihoods, generic and SDSS-DR14

---
+++ cobaya/likelihoods/lya/generic.py
@@ -0,0 +1,5 @@
+from cobaya.likelihoods.base_classes import LYA
+
+
+class generic(LYA):
+    pass
---
+++ cobaya/likelihoods/lya/sdss_dr14_lya.py
@@ -0,0 +1,8 @@
+from cobaya.likelihoods.base_classes import LYA
+
+
+class sdss_dr14_lya(LYA):
+    r"""
+    Likelihood of the compressed Lyman-alpha flux 1D power spectrum from SDSS-DR14 \cite{Chabanier:2018rga}.
+    """
+    pass
