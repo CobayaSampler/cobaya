2387e32ed32d6c7162440305751ee6075427c4e3
Antony Lewis <antony@cosmologist.info>
2023-04-04 14:39:09 +0100

    typo

--- cobaya/__init__.py
+++ cobaya/__init__.py
@@ -14,6 +14,6 @@ if sys.version_info < (3, 8):
 
 __author__ = "Jesus Torrado and Antony Lewis"
 __version__ = "3.3.1"
-__obsolete__ = Falses
+__obsolete__ = False
 __year__ = "2023"
 __url__ = "https://cobaya.readthedocs.io"
