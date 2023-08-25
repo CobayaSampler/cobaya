44ed5ac9a1e0ddeb49629399df3db878184c2535
torradocacho <JesusTorrado@users.noreply.github.com>
2023-08-25 12:15:59 +0200

    rtd test [skip travis]

--- cobaya/yaml.py
+++ cobaya/yaml.py
@@ -23,7 +23,8 @@ import numpy as np
 if os.environ.get("READTHEDOCS"):
     sys_path_copy = copy(sys.path)
     sys.path = [p for p in sys.path if p]
-    from importlib import import_module
+    from importlib import import_module, invalidate_caches
+    invalidate_caches()
     pyyaml = import_module("yaml", package="yaml")
     sys.path = sys_path_copy
 else:
