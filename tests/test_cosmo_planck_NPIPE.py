9451d5b319285a305dced302916ad3a578113aa6
jesus <jesus@debian.debian>
2023-09-06 15:17:29 +0200

    npipe: removed installation from test and wrapped error

--- tests/test_cosmo_planck_NPIPE.py
+++ tests/test_cosmo_planck_NPIPE.py
@@ -1,11 +1,10 @@
 import numpy as np
-import pytest
 
-from cobaya.install import install
 from cobaya.model import get_model
 from cobaya.yaml import yaml_load
 
 from .common import process_packages_path
+from .conftest import install_test_wrapper
 
 yaml = r"""
 theory:
@@ -35,15 +34,11 @@ params:
 """
 
 
-def test_planck_NPIPE_p_CamSpec_camb_install(packages_path):
+def test_planck_NPIPE_p_CamSpec_camb_install(packages_path, skip_not_installed):
     packages_path = process_packages_path(packages_path)
     info = yaml_load(yaml)
-    result = install(info, path=packages_path)
-    if result is False:
-        raise pytest.xfail("Some component(s) skipped")
-
-    model = get_model(info, packages_path=packages_path)
+    model = install_test_wrapper(
+        skip_not_installed, get_model, info, packages_path=packages_path)
     pars = (0.99818025, 10.35947284, 18.67072461, 7.54932654, 0.83715482,
             0.94987418, 1.23385364, 0.98781552, 1.013345)
-
     assert np.isclose(model.logposterior(pars).logpost, -5889.873, rtol=1e-4)
