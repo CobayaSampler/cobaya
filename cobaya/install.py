7dc7f0ef034d577c17765fd27bd9e9e658742e62
Antony Lewis <antony@cosmologist.info>
2023-10-20 10:20:15 +0100

    pip_install using current env by default

--- cobaya/install.py
+++ cobaya/install.py
@@ -527,12 +527,11 @@ def pip_install(packages, upgrade=False, logger=None, options=(), **kwargs):
     if upgrade:
         cmd += ['--upgrade']
     cmd += list(options)
+    if "env" not in kwargs:
+        kwargs["env"] = os.environ
     # Assume that if the user has installed Cobaya on the system-wide Python,
     # then it is OK to overwrite system packages:
-    kwargs = deepcopy(kwargs)
-    if "env" not in kwargs:
-        kwargs["env"] = {}
-    kwargs["env"].update({"PIP_BREAK_SYSTEM_PACKAGES": "1"})
+    kwargs["env"] = dict(kwargs["env"], PIP_BREAK_SYSTEM_PACKAGES="1")
     res = subprocess.call(cmd + packages, **kwargs)
     if res:
         msg = f"pip: error installing packages '{packages}'"
