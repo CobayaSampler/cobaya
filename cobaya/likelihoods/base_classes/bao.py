6781803bbee53e05c1d846b9d8f5e05b722795a6
Kushal Lodha <lightingeagle@gmail.com>
2024-01-09 22:08:39 +0900

    Updating bao_data's github release to v2.3

--- cobaya/likelihoods/base_classes/bao.py
+++ cobaya/likelihoods/base_classes/bao.py
@@ -156,7 +156,7 @@ class BAO(InstallableLikelihood):
     type = "BAO"
 
     install_options = {"github_repository": "CobayaSampler/bao_data",
-                       "github_release": "v2.2"}
+                       "github_release": "v2.3"}
 
     prob_dist_bounds: Optional[Sequence[float]]
     measurements_file: Optional[str] = None
