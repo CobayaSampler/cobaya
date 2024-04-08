09f43aebde175d25e50f6c54a65797c5e621544c
Antony Lewis <antony@cosmologist.info>
2024-04-08 08:25:31 +0200

    cleanup des y5

--- cobaya/likelihoods/sn/desy5.py
+++ cobaya/likelihoods/sn/desy5.py
@@ -14,8 +14,6 @@ class desy5(pantheonplus):
 
     def init_params(self, ini):
         self.twoscriptmfit = False
-        # self.pecz = 0.
-        # self.has_third_var = False
         data_file = os.path.normpath(os.path.join(self.path, ini.string("data_file")))
         self._read_data_file(data_file)
         self.covs = {}
@@ -25,11 +23,6 @@ class desy5(pantheonplus):
                 os.path.join(self.path, ini.string('%s_covmat_file' % name)))
         self.alphabeta_covmat = False
         self.pre_vars = self.mag_err ** 2  # diagonal component
-        # argsort = np.argsort(self.zcmb)
-        # for col in self.cols:
-        #    setattr(self, col, getattr(self, col)[argsort])
-        # for name, cov in self.covs.items():
-        #    self.covs[name] = cov[np.ix_(argsort, argsort)]
         self.inverse_covariance_matrix()
         if not self.use_abs_mag:
             self._marginalize_abs_mag()
@@ -38,7 +31,6 @@ class desy5(pantheonplus):
     def _read_data_file(self, data_file):
         self.log.debug('Reading %s' % data_file)
         sep = ','
-        supernovae = {}
         self.names = []
         oldcols = ['zhd', 'zhel', 'mu', 'muerr_final']
         self.cols = ['zcmb', 'zhel', 'mag', 'mag_err']
