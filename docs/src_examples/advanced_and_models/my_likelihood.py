911dc26ed5637ab7441a72b0f98c5e24abc907e7
Adam Ormondroyd <52655393+AdamOrmondroyd@users.noreply.github.com>
2024-06-16 22:12:44 +0100

    on cartesian coordinater -> in cartesian coordinates (#362)

--- docs/src_examples/advanced_and_models/my_likelihood.py
+++ docs/src_examples/advanced_and_models/my_likelihood.py
@@ -4,7 +4,7 @@ from scipy import stats
 
 def gauss_ring_logp(x, y, mean_radius=1, std=0.02):
     """
-    Defines a gaussian ring likelihood on cartesian coordinater,
+    Defines a gaussian ring likelihood in cartesian coordinates,
     around some ``mean_radius`` and with some ``std``.
     """
     return stats.norm.logpdf(np.sqrt(x**2 + y**2), loc=mean_radius, scale=std)
