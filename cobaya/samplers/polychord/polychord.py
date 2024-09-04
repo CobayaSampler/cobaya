0cb21ac1f81a56b2a8589290434945f4bec928c7
Giacomo Galloni <giacomo.galloni@unife.it>
2024-09-04 12:35:32 +0200

    Add `str` when allowed

--- cobaya/samplers/polychord/polychord.py
+++ cobaya/samplers/polychord/polychord.py
@@ -55,13 +55,13 @@ class polychord(Sampler):
 
     # variables from yaml
     do_clustering: bool
-    num_repeats: int
+    num_repeats: Union[int, str]
     confidence_for_unbounded: float
     callback_function: Callable
     blocking: Any
     measure_speeds: bool
     oversample_power: float
-    nlive: NumberWithUnits
+    nlive: Union[str, NumberWithUnits]
     path: str
     logzero: float
     max_ndead: int
